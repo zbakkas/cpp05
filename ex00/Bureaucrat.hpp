bac13fccc356255c054070a4e92fd5954f9d7170
Zouhir Bakkas <zouhirbakkas@gmail.com>
2025-01-02 22:11:41 +0100

    ex00

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -1,12 +1,39 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
 
 class Bureaucrat
 {
-private:
-    
-public:
-    Bureaucrat();
-    Bureaucrat(const Bureaucrat& bureaucrat);
-    Bureaucrat &operator =(const Bureaucrat &bureaucrat);
-    ~Bureaucrat();
+    private:
+        const std::string name;
+        int grade;
+
+    public:
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        Bureaucrat();
+        Bureaucrat(const Bureaucrat& bureaucrat);
+        Bureaucrat &operator =(const Bureaucrat &bureaucrat);
+        ~Bureaucrat();
+
+        Bureaucrat(std::string name, int grade);
+        std::string getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+        
 };
 
+std::ostream &operator <<(std::ostream &out, const Bureaucrat &bureaucrat);
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,40 @@
-int main()
+#include "Bureaucrat.hpp"
+
+int main(void)
 {
-    
+	try
+	{
+		Bureaucrat a("Student", 148);
+		std::cout << a << std::endl;
+		std::cout << "Grade -> " << a.getGrade() << std::endl;
+		a.decrementGrade();
+		a.decrementGrade();
+		std::cout << "Grade -> " << a.getGrade() << std::endl;
+		a.decrementGrade();
+	}
+	catch(const std::exception &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat b;
+		std::cout << b << std::endl;
+		b.decrementGrade();
+		b.decrementGrade();
+		b.decrementGrade();
+		std::cout << "Grade -> " << b.getGrade() << std::endl;
+		b.incrementGrade();
+		b.incrementGrade();
+		b.incrementGrade();
+		std::cout << "Grade -> " << b.getGrade() << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	Bureaucrat x("Copy", 115);
+	Bureaucrat y = x;
+	std::cout << y << std::endl;
+	return (0);
 }
