a215d65bcf828eb5471f360bf59b6353f0d7d5c6
Zouhirborali <zouhirbakkas@gmail.com>
2025-01-02 15:54:47 +0100

    add

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,12 @@
+
+class Bureaucrat
+{
+private:
+    
+public:
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat& bureaucrat);
+    Bureaucrat &operator =(const Bureaucrat &bureaucrat);
+    ~Bureaucrat();
+};
+
